e67717cc3a677251b7056624043d38aa35e1b08a
Nick Durcholz <nick.durcholz@gmail.com>
2014-07-25 13:43:26 -0400

    correct AssemblyTitle

--- ArgumentsParser/Properties/AssemblyInfo.cs
+++ ArgumentsParser/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("AppCommons")]
-[assembly: AssemblyDescription("http://github.com/nickdurcholz/app-commons")]
+[assembly: AssemblyTitle("ArgumentsParser")]
+[assembly: AssemblyDescription("http://github.com/nickdurcholz/arguments-parser")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d56efc40-569b-41a4-845e-f3f3b172554e")]
